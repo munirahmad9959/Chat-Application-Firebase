017de54fea88cd77ab61382651af37709f580957
ZavAhmed <2022cs614@student.uet.edu.pk>
2024-07-21 23:34:31 +0500

    Login Page Started

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 import List from './components/list/List'
 import Chat from './components/chat/Chat'
 import Details from './components/details/Details'
+import Login from './components/login/Login'
 
 function App() {
   return (
     <div className='container'>
-      <List />
-      <Chat />
-      <Details />
+
+      {
+        user ? (
+          <>
+            <List />
+            <Chat />
+            <Details />
+          </>
+        )
+          : <Login />
+      }
     </div>
   )
 }
